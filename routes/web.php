16df3619850927ac2e79fac84c909910a01da5d6
Moeen Uddin <m.moeen.u9@gmail.com>
2025-11-13 19:20:39 +0600

    Update: fix route on web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Backend\AuthController;
 use App\Http\Controllers\Frontend\HomeController;
-use Illuminate\Support\Facades\App;
+use App\Http\Controllers\Backend\HomeController as BackendHomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\Frontend\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 Route::prefix('admin')->group(function () {
@@ -28,6 +28,6 @@ Route::prefix('admin')->group(function () {
     Route::post('/admin-login', [AuthController::class, 'authenticate'])->name('admin.authenticate');
 
     Route::middleware('admin.auth')->group(function () {
-        Route::get('/admin-dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/admin-dashboard', [BackendHomeController::class, 'index'])->name('admin.dashboard');
     });
 });
