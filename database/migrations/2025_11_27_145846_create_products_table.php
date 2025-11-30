cacf12f08d658a0542ebfda11cd960f7b62bfaa8
Moeen Uddin <m.moeen.u9@gmail.com>
2025-11-30 18:31:02 +0600

    Update: set product table column name

--- database/migrations/2025_11_27_145846_create_products_table.php
+++ database/migrations/2025_11_27_145846_create_products_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
             $table->string('product_name');
             $table->string('product_slug');
             $table->string('product_img');
+            $table->string('product_stock');
             $table->boolean('product_status')->default(0);
             $table->decimal('product_price', 10, 2);
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
